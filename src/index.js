7c3a5ddbb8d1e1d70053be63fea92b74f9f89251
flaviamenck <flavia-menck@hotmail.com>
2025-06-01 20:16:21 -0400

    desafio completo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-let nome = "Flavia"
-console.log(nome);
+let nomeHeroi = "Sage";
+let xpHeroi = 10005;
+let classificao = "";
+
+switch (true) {
+    case (xpHeroi < 1000):
+        classificao = "Ferro";
+        break;
+    case (xpHeroi >= 1001 && xpHeroi < 2000):
+        classificao = "Bronze";
+        break;
+    case (xpHeroi >= 2001 && xpHeroi < 5000):
+        classificao = "Prata";
+        break;
+    case (xpHeroi >= 5001 && xpHeroi < 7000):
+        classificao = "Ouro";
+        break;
+    case (xpHeroi >= 7001 && xpHeroi < 8000):
+        classificao = "Platina";
+        break;
+    case (xpHeroi >= 8001 && xpHeroi < 9000):
+        classificao = "Ascendente";
+        break;
+    case (xpHeroi >= 9001 && xpHeroi < 10000):
+        classificao = "Imortal";
+        break;
+    case (xpHeroi >= 10001):
+        classificao = "Radiante";
+        break;
+    default:
+        console.log("Nenhum XP registrado.");
+}
+
+console.log("O herói de nome " + nomeHeroi + " está no nível de " + classificao + "!");
